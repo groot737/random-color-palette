a6c90bb5cddbeaa65ddfc56bc0bb425570a92b16
groot737 <giorgiquchuloria7@gmail.com>
2023-07-09 17:03:39 +0400

    colormind api has issues, I created color generator

--- app.js
+++ app.js
@@ -1,57 +1,60 @@
-function copy(element) {
+function generateRandomColor() {
+    // Generate random values for red, green, and blue between 0 and 255
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    // Convert the RGB values to a hexadecimal color code
+    const colorCode = ((red << 16) | (green << 8) | blue).toString(16);
+    const paddedColorCode = colorCode.padStart(6, '0');
+    return '#' + paddedColorCode;
+  }
+  
+  function copy(element) {
     navigator.clipboard.writeText(`"${element}"`)
     document.getElementsByTagName("button")[0].style.display = "block";
     document.getElementsByTagName("span")[0].innerText = element;
-
+  
     setTimeout(() => {
-        document.getElementsByTagName("button")[0].style.display = "none";
+      document.getElementsByTagName("button")[0].style.display = "none";
     }, 3000);
-}
-let colors;
-
-function fetchColor() {
-    let box = document.getElementById('parent')
-
-    var url = "http://colormind.io/api/";
-    var data = {
-        model: "default",
-        input: [[44, 43, 44], [90, 83, 82], "N", "N", "N"]
+  }
+  
+  let colors;
+  
+  function generateColors() {
+    let box = document.getElementById('parent');
+    box.innerHTML = '';
+    colors = [];
+  
+    // Generate 5 random colors
+    for (let i = 0; i < 5; i++) {
+      const color = generateRandomColor();
+      colors.push(color);
+      box.innerHTML += `
+        <div class="item" onclick='copy("${color}")')'>
+          <div class="item-box">
+            <div class="color-box" style="background-color: ${color}"></div>
+            <h3>${color}</h3>
+          </div>
+        </div>
+      `;
     }
+  }
+  
 
-    var http = new XMLHttpRequest();
+  generateColors();
+  
 
-    http.onreadystatechange = function () {
-        if (http.readyState == 4 && http.status == 200) {
-            var palette = JSON.parse(http.responseText).result;
-            const hexCodes = palette.map(palette => '#' + palette.map(c => c.toString(16).padStart(2, '0')).join(''));
-            box.innerHTML = ''
-            colors = hexCodes;
-            hexCodes.forEach(element => {
-                box.innerHTML += `
-                  <div class="item" onclick='copy("${element}")')'>
-                    <div class="item-box">
-                      <div class="color-box" style="background-color: ${element}"></div>
-                      <h3>${element}</h3>
-                    </div>
-                  </div>
-               `
-            })
-        }
+  document.getElementById('generate').addEventListener('click', () => {
+    generateColors();
+  });
+  
+
+  document.addEventListener('keydown', event => {
+    if (event.key === 'c') {
+      copy('palette')
+      navigator.clipboard.writeText(colors);
+    } else if (event.key == ' '){
+        generateColors()
     }
-
-    http.open("POST", url, true);
-    http.send(JSON.stringify(data));
-}
-fetchColor()
-
-document.getElementById('generate').addEventListener('click', () => {
-    fetchColor()
-})
-document.addEventListener('keydown', event => {
-    if (event.key === ' ') { 
-        fetchColor()
-    } else if (event.key === 'c'){
-        navigator.clipboard.writeText(`${colors}`)
-        copy('palette')
-    }
- });
+  });
